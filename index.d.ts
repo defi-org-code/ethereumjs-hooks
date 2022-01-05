83a8bcbafb3fb50cb61b3182916065f45f112533
DanielZlotin <6660239+DanielZlotin@users.noreply.github.com>
2022-01-05 12:42:50 +0200

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module "ethereumjs-hooks" {
+declare module "@defi.org/ethereumjs-hooks" {
   /**
    * Modifies EVM opcode 0x43 (block.number) to return `block.number + n`. Does not affect web3, only solidity/evm runtime during execution of `fn`.
    *
